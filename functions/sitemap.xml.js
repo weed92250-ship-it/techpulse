919efbf7169822660f637c3ee4b56de938df4144
weed92250-ship-it <weed92250@gmail.com>
2026-09-06 20:29:37 +0300

    Create sitemap.xml.js to serve XML sitemap
    Generate XML sitemap with articles from the database.

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -1,1 +1,18 @@
+const SITE_URL = 'https://techpulseon.pages.dev';
 
+export async function onRequestGet({ env }) {
+  const { results } = await env.DB.prepare('SELECT id, date FROM articles ORDER BY date DESC').all();
+  const urls = results
+    .map(r => `  <url>\n    <loc>${SITE_URL}/article/${r.id}</loc>\n    <lastmod>${r.date}</lastmod>\n  </url>`)
+    .join('\n');
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${SITE_URL}/</loc>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+${urls}
+</urlset>`;
+  return new Response(xml, { headers: { 'content-type': 'application/xml; charset=UTF-8' } });
+}
